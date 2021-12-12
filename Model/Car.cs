50a4bed29e5ee75d9c8540fc32fd0c27034b7ae3
Moises1Martinez <moisesantoniom@gmail.com>
2021-12-12 07:51:54 -0600

    Parte Moises Martinez. Creando los modelos que componen la Base de Datos.

--- Model/Car.cs
+++ Model/Car.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParcialComputo3.Models
 {
     public class Car
     {
+        [Key]
+        public int idCar { get; set; }
+        public string modelCar { get; set; }
+        public string colorCar { get; set; }
+        public string yearCar { get; set; }
+        public string typeCar { get; set; }
+        
+        // Instanciar la Key de la clase Brand.
+        public int idBrand { get; set; }
+
     }
 }
