fe15d7457f2e10627a437cafd10e34cf0f55c5f8
Cesar Martinez <oliver-cesar@hotmail.com>
2021-12-12 13:42:32 -0600

    Cambios Finales.

---
+++ Migrations/20211212193151_PrimerMigracion.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ParcialComputo3.Migrations
+{
+    public partial class PrimerMigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    idBrand = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nameBrand = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.idBrand);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    idCar = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    modelCar = table.Column<string>(type: "text", nullable: true),
+                    colorCar = table.Column<string>(type: "text", nullable: true),
+                    yearCar = table.Column<string>(type: "text", nullable: true),
+                    typeCar = table.Column<string>(type: "text", nullable: true),
+                    idBrand = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.idCar);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Brands");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
